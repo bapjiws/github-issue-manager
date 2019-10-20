e299d712eefda3fc716625c2b9a49f776de60fce
Alex Boklin <alex.boklin@gmail.com>
2019-10-20 23:52:00 +0200

    Set up the WebSocket communication; add TODOs

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 
 const express = require('express');
 const app = express();
+const WebSocket = require('ws');
 
-app.get('/issues', function (req, res) {
+const wss = new WebSocket.Server({ port: 8080 });
+
+let websocket;
+
+wss.on('connection', ws => {
+  websocket = ws;
+
+  // TODO: don't need this one.
+  ws.on('message', message => {
+    console.log('received: %s', message);
+  });
+});
+
+app.get('/issues', (req, res) => {
   console.log('RECEIVED');
+  websocket.send('Accessed /issues');
+  // TODO: just send 200.
   res.send('Hello, GitHub!');
 });
 
-app.listen(3000, () => console.log('Listening...'));
+app.listen(5000, () => console.log('Listening...'));
--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const ws = new WebSocket('ws://localhost:8080');
+
+  useEffect(() => {
+    ws.onopen = event => {
+      console.log("WebSocket is open now.");
+    };
+
+    ws.onmessage = event => {
+      console.log("WebSocket message received:", event);
+    };
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default App;
