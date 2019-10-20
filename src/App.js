592e349f826f4759cf0b0a62332b8d74ef77c375
Alex Boklin <alex.boklin@gmail.com>
2019-10-21 01:24:56 +0200

    Set up Apollo client and create Issues component with a query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import { Issues } from './Issues';
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+});
 
 const App = () => {
   const ws = new WebSocket('ws://localhost:8080');
@@ -16,22 +24,25 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Issues />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 };
 
---
+++ src/Issues.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+// TODO: make `owner` and `name` dynamic.
+const OPEN_ISSUES = gql`
+  {
+    repository(owner: "bapjiws", name: "github-issue-manager") {
+      issues(last: 20, states: OPEN) {
+        edges {
+          node {
+            title
+            url
+            bodyText
+            assignees(last: 20) {
+              edges {
+                node {
+                  login
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Issues = () => {
+  const { loading, error, data } = useQuery(OPEN_ISSUES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log('data:', data);
+
+  return <div>Issues: </div>;
+};
