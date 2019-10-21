abcb1fb09b080e85292c8b505146fc0eea0a19fd
Alex Boklin <alex.boklin@gmail.com>
2019-10-21 23:12:18 +0200

    Add rel="noopener noreferrer" to the links

--- src/Issue.js
+++ src/Issue.js
@@ -5,7 +5,13 @@ import './Issue.css';
 export const Issue = ({ avatarUrl, bodyText, createdAt, id, title, url }) => (
   <div key={id} className="Issue">
     <div className="Issue-info">
-      <a href={url} target="_blank">{title}</a>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {title}
+      </a>
       {bodyText}
       <br/>
       {new Date(createdAt).toDateString()}
