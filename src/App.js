17a1cd8b6892d2acd7a61ee19f8036192f0fb87b
Alex Boklin <alex.boklin@gmail.com>
2019-10-21 23:14:16 +0200

    Clean unused elements and their styles

--- src/App.js
+++ src/App.js
@@ -30,19 +30,7 @@ const App = () => {
     <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <Issues />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     </ApolloProvider>
