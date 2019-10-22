bd884f5a8da31ab8b0aca8f71fa162d88fb61406
Alex Boklin <alex.boklin@gmail.com>
2019-10-22 21:40:07 +0200

    Use named imports for IssueCard

--- src/IssueCard.js
+++ src/IssueCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   card: {
