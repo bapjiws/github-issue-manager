75df794de0f870c1b442c55de24aee36a638493d
Alex Boklin <alex.boklin@gmail.com>
2019-10-23 20:12:00 +0200

    Use CircularProgress as a loading indicator

--- src/Issues.js
+++ src/Issues.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import {
+  CircularProgress,
+  Typography
+} from '@material-ui/core';
 
 import { Issue } from './Issue';
 
@@ -55,7 +58,7 @@ export const Issues = () => {
     }
   }, [data]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <CircularProgress />;
   if (error) return <p>Error :(</p>;
 
   const { repository: { issues } } = data;
